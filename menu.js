903b85948d80670f0945aeefd1227561d54f99d1
SixteenByNein <gunnarlorenzoyoung@gmail.com>
2026-04-05 16:50:03 -0600

    Fixed hidden menu button

--- menu.js
+++ menu.js
@@ -3,14 +3,14 @@ function toggleMenu()
 
     var menu = document.getElementById("mobileMenu");
 
-    if(menu.style.display == "block")
+    if(menu.style.display == "flex")
     {
     menu.style.display="none";
     }
 
     else
     {
-    menu.style.display="block";
+    menu.style.display="flex";
     }
 
 }
