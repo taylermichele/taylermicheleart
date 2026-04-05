bee9fca8558233237fcd043e13a5ace571b73c09
SixteenByNein <gunnarlorenzoyoung@gmail.com>
2026-04-05 17:26:52 -0600

    Added Favicon. Fixed toggle menu. Added dynamic toggle button. fixed side-by-side display in portrait mode.

--- menu.js
+++ menu.js
@@ -2,15 +2,20 @@ function toggleMenu()
 {
 
     var menu = document.getElementById("mobileMenu");
+    var menuSwitch = document.getElementById("dropdownButton");
 
     if(menu.style.display == "flex")
     {
     menu.style.display="none";
+
+    menuSwitch.innerHTML = "<h1>≡</h1>"
     }
 
     else
     {
     menu.style.display="flex";
+
+    menuSwitch.innerHTML = "<h1>⤫</h1>"
     }
 
 }
